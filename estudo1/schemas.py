418be177deb02dac9cda764a73d7d42ca7d17e9b
Kaio Masaru Nakazono <kmn1405@gmail.com>
2024-06-11 18:31:52 -0300

    criado arquivo cars.json e função para criar objetos Car numa lista em schema.py

--- estudo1/schemas.py
+++ estudo1/schemas.py
@@ -1,3 +1,4 @@
+import json
 from pydantic import BaseModel
 
 class Car(BaseModel):
@@ -6,3 +7,9 @@ class Car(BaseModel):
    fuel: str|None = 'eletric'  
    doors: int
    transmission: str|None = 'auto'
+
+
+def load_db() -> list[Car]:
+   """Load a list of Car objects from a JSON file"""
+   with open("cars.json") as f:
+      return [Car.model_validate(obj) for obj in json.load(f)]
